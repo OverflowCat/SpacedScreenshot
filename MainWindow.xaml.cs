53e7349e312feefed21ebcc808c05b4cdca3991b
猫猫 <overflowcat@gmail.com>
2021-11-05 19:46:39 +0800

    添加项目文件。

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Threading;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace SpacedScreenshot
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public int count = 0;
+        public DispatcherTimer timer = new DispatcherTimer();
+        private string filenamePrefix;
+        public MainWindow()
+        {
+            InitializeComponent();
+            timer.Tick += new EventHandler(TakeScreenshot);
+        }
+
+        private void TakeScreenshot(object sender, EventArgs e)
+        {
+            /* 
+               Code copied from StackOverflow.
+               https://stackoverflow.com/a/363008
+               CC BY-SA 3.0
+            */
+
+            //Create a new bitmap.
+            Bitmap bmpScreenshot = new Bitmap(Screen.PrimaryScreen.Bounds.Width,
+                                           Screen.PrimaryScreen.Bounds.Height
+                                           );
+
+            // Create a graphics object from the bitmap.
+            Graphics gfxScreenshot = Graphics.FromImage(bmpScreenshot);
+
+            // Take the screenshot from the upper left corner to the right bottom corner.
+            gfxScreenshot.CopyFromScreen(Screen.PrimaryScreen.Bounds.X,
+                                        Screen.PrimaryScreen.Bounds.Y,
+                                        0,
+                                        0,
+                                        Screen.PrimaryScreen.Bounds.Size,
+                                        CopyPixelOperation.SourceCopy);
+
+            // Save the screenshot to the specified path that the user has chosen.
+            string folderPath = path_tb.Text;
+            if (!folderPath.EndsWith("\\"))
+            {
+                folderPath += "\\";
+            }
+
+            bmpScreenshot.Save($"{folderPath}{filenamePrefix}-{count}-{DateTime.Now:yyyyMMdd-hhmmss}.png", System.Drawing.Imaging.ImageFormat.Png);
+            count++;
+            counter_label.Content = count.ToString();
+        }
+
+        private void start_btn_Click(object sender, RoutedEventArgs e)
+        {
+            // 获取文本框中的时间间隔值
+            int interval;
+            try
+            {
+                interval = Int32.Parse(interval_tb.Text);
+                Console.WriteLine(interval);
+                filenamePrefix = filenamePrefix_tb.Text;
+                timer.Interval = new TimeSpan(0, 0, interval);
+                start_btn.IsEnabled = false;
+                end_btn.IsEnabled = true;
+                counter_label.Content = "0";
+                count = 0;
+                timer.Start();
+            }
+            catch
+            {
+                System.Windows.Forms.MessageBox.Show("请输入正确的时间间隔！");
+            }
+        }
+
+        private void end_btn_Click(object sender, RoutedEventArgs e)
+        {
+            timer.Stop();
+            end_btn.IsEnabled = false;
+            start_btn.IsEnabled = true;
+        }
+    }
+}
